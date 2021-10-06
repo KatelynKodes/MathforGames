0b9e17c96cd6781e0fcfe95f7dcef16bd5ccd532
Katelyn <katelynwest225@gmail.com>
2021-10-06 16:53:05 -0500

    Set up base functions and scene class

---
+++ MathForGames/Engine.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathForGames
+{
+    class Engine
+    {
+        private static bool _shouldApplicationclose = false;
+        private static int _currentSceneIndex;
+        private Scene[] _scenes = new Scene[0];
+
+        /// <summary>
+        /// Called to begin the application
+        /// </summary>
+        public void Run()
+        {
+            //Call start for the entire application
+            Start();
+
+            //Loop until application is told to close
+            while (!_shouldApplicationclose)
+            {
+                Update();
+                Draw();
+            }
+
+            //Calll at the end of the entire application.
+            End();
+        }
+
+        /// <summary>
+        /// Called when the application starts
+        /// </summary>
+        private void Start()
+        {
+            _scenes[_currentSceneIndex].Start();
+        }
+
+        /// <summary>
+        /// Called everytime the game loops
+        /// </summary>
+        private void Update()
+        {
+            _scenes[_currentSceneIndex].Update();
+
+        }
+
+        /// <summary>
+        /// Called At the end of the application
+        /// </summary>
+        private void End()
+        {
+            _scenes[_currentSceneIndex].End();
+
+        }
+
+        /// <summary>
+        /// Called everytime the game loops to update visuals
+        /// </summary>
+        private void Draw()
+        { 
+            _scenes[_currentSceneIndex].Draw();
+
+        }
+
+        /// <summary>
+        /// Adds a new scene to the engines scene array
+        /// </summary>
+        /// <param name="scene"></param>
+        /// <returns></returns>
+        public int AddScene(Scene scene)
+        {
+            //Create Temporary Array
+            Scene[] TempArray = new Scene[_scenes.Length + 1];
+
+            //Copy all values into temporary array
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                TempArray[i] = _scenes[i];
+            }
+
+            //Set the last index to be the new scene
+            TempArray[_scenes.Length] = scene;
+
+            //set the old array to the new array
+            _scenes = TempArray;
+
+            //Return the last index
+            return _scenes.Length - 1;
+        }
+    }
+}

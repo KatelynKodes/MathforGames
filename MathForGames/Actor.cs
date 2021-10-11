cfc8364cc830aff333a50c137c30ba34a0c3f9bd
Katelyn <katelynwest225@gmail.com>
2021-10-11 12:53:02 -0500

    Made the player icon move across the screen

--- MathForGames/Actor.cs
+++ MathForGames/Actor.cs
@@ -10,6 +10,12 @@ namespace MathForGames
         private char _icon;
         private string _name;
         private Vector2 _position;
+        private bool _started;
+
+        public bool Started
+        {
+            get { return _started;}
+        }
 
         public Vector2 GetPosition
         {
@@ -26,12 +32,12 @@ namespace MathForGames
 
         public virtual void Start()
         {
-            
+            _started = true;
         }
 
         public virtual void Update()
         {
-            _position.X = GetPosition.X + 1;
+            _position.X = _position.X + 1;
         }
 
         public virtual void Draw()
